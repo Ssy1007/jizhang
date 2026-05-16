ae5f6ed51cd90b4180018044457a6b010a89c391
Ssy1007 <892434834@qq.com>
2026-05-16 20:38:30 +0800

    Fix WebView: SSL handler, back button, better settings

--- twa-project/app/src/main/java/com/jizhang/app/MainActivity.java
+++ twa-project/app/src/main/java/com/jizhang/app/MainActivity.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.webkit.WebSettings;
+import android.webkit.SslErrorHandler;
+import android.net.http.SslError;
 import android.view.WindowManager;
 
 public class MainActivity extends Activity {
+    private WebView webView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,14 +20,22 @@ public class MainActivity extends Activity {
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         );
 
-        WebView webView = new WebView(this);
-        webView.setWebViewClient(new WebViewClient());
+        webView = new WebView(this);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                // 接受所有 SSL 证书（仅用于信任的 GitHub Pages）
+                handler.proceed();
+            }
+        });
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
         settings.setDomStorageEnabled(true);
         settings.setAllowFileAccess(false);
         settings.setDatabaseEnabled(true);
         settings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        settings.setUserAgentString(settings.getUserAgentString() + " JizhangApp/1.0");
 
         webView.loadUrl("https://ssy1007.github.io/jizhang/");
         setContentView(webView);
@@ -31,7 +43,6 @@ public class MainActivity extends Activity {
 
     @Override
     public void onBackPressed() {
-        WebView webView = (WebView) findViewById(android.R.id.content);
         if (webView != null && webView.canGoBack()) {
             webView.goBack();
         } else {
